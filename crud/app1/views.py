937cf644f95c7bc0978107967e820edd422bf4e0
Mahadi <smmahadi01910048684@gmail.com>
2024-11-28 23:37:08 +0600

    well

--- crud/app1/views.py
+++ crud/app1/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render,HttpResponse,HttpResponseRedirect
+from .models import *
+
+def signup(request):
+    return render(request,"signup.html")
+
